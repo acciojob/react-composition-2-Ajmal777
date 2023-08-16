b529da2b3fe45514b5f09d0a535244e31c3b65cb
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-08-16 14:19:20 +0000

    ab toh kaam kar ja bhai

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,17 +3,13 @@ import React , {useState} from "react";
 const Modal = () =>{
     const [showModal, setShowModal] = useState(false);
 
-    const onClose = () =>{
-        setShowModal(false);
-    }
-
     return (
-        <div className="model-overlay">
+        <div className="model">
             <button onClick={()=> setShowModal(true)}>Show Modal</button>
             {
                 showModal && 
-                    <div className="model">
-                        <button className="model-close" onClick={onClose}>Close</button>
+                    <div className="model-overlay">
+                        <button className="model-close" onClick={() => setShowModal(false)}>Close</button>
                         <p>This is the content of the modal.</p>
                     </div>
             }
